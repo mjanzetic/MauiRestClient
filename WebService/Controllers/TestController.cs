ba08ce3c20a9aa51c970eff1904a1bcade4ad37d
MJ <marius.janzetic@gmail.com>
2025-01-14 16:16:52 +0100

    added further http methods to respond to

--- WebService/Controllers/TestController.cs
+++ WebService/Controllers/TestController.cs
@@ -7,7 +7,13 @@ namespace WebService.Controllers;
 public class TestController : ControllerBase
 {
     [HttpGet(Name = "GetOk")]
-    public IActionResult Get()
+    [HttpHead(Name = "HeadOk")]
+    [HttpDelete(Name = "DeleteOk")]
+    [HttpOptions(Name = "OptionsOk")]
+    [HttpPatch(Name = "PatchOk")]
+    [HttpPost(Name = "PostOk")]
+    [HttpPut(Name = "PutOk")]
+    public IActionResult Respond()
     {
         return Ok();
     }
